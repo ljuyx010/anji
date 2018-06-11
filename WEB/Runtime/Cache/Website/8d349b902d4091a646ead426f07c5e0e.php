575ffb1b70b6bfe9b88fad9c28778aa37118ce2a
ljuyx010 <34336224+ljuyx010@users.noreply.github.com>
2018-06-11 21:53:08 +0800

    06.11 21:50

--- WEB/Runtime/Cache/Website/8d349b902d4091a646ead426f07c5e0e.php
+++ WEB/Runtime/Cache/Website/8d349b902d4091a646ead426f07c5e0e.php
@@ -25,7 +25,7 @@
                 <div class="ibox-title">
                     <h5>权限管理</h5>
                     <div class="ibox-tools">
-                        <a data-toggle="modal" onclick="add(0)" data-target="#myModal" class="btn btn-primary btn-xs"><i class="fa fa-plus"></i> 添加权限</a>
+                        <a data-toggle="modal" onclick="add(0,'',0)" data-target="#myModal" class="btn btn-primary btn-xs"><i class="fa fa-plus"></i> 添加权限</a>
                     </div>
                 </div>
                 <div class="ibox-content">
@@ -36,7 +36,7 @@
                             <tbody>
                             <tr>
                                 <th class="project-title">
-                                    权限名
+                                    <a href="JavaScript:;">权限名</a>
                                 </th>
                                 <th class="project-completion">
                                     权限规则                                  
@@ -50,7 +50,7 @@
                             </tr>
                             <?php if(is_array($rule)): foreach($rule as $key=>$v): ?><tr>
                                 <td class="project-title">
-                                    <a href="JavaScript:;"><?php echo ($v["title"]); ?></a>
+                                    <?php echo ($v["pix"]); echo ($v["title"]); ?>
                                 </td>
                                 <td class="project-completion">
                                     <?php echo ($v["name"]); ?>                                   
@@ -59,7 +59,7 @@
                                     <small><?php echo ($v["type"]); ?></small>                                    
                                 </td>
                                 <td class="project-actions">
-                                <a data-toggle="modal" data-target="#myModal" onclick="add(<?php echo ($v["type"]); ?>)" class="btn btn-success btn-sm"><i class="fa fa-plus"></i> 添加子项</a>
+                                <a data-toggle="modal" data-target="#myModal" onclick="add(<?php echo ($v["type"]); ?>,<?php echo ($v["id"]); ?>,[<?php echo ($v["sort"]); ?>])" class="btn btn-success btn-sm"><i class="fa fa-plus"></i> 添加子项</a>
                                 <a data-toggle="modal" data-target="#myModal" onclick="edit(<?php echo ($v["id"]); ?>)" class="btn btn-info btn-sm"><i class="fa fa-pencil"></i> 编辑 </a>
                                 <a href="<?php echo U('Admin/delete',array('id'=>$v['id']));?>" class="btn btn-warning btn-sm" onclick="return confirm('确定要删除吗？')"><i class="fa fa-trash"></i> 删除 </a>
                                 </td>
@@ -94,6 +94,8 @@
                     <div class="col-sm-6">                                
                         <input type="text" class="form-control" name="name" value="">
                         <input type="hidden" class="form-control" name="type" value="">
+                        <input type="hidden" class="form-control" name="sort" value="">
+                        <input type="hidden" class="form-control" name="fid" value="">
                         <input type="hidden" class="form-control" name="id" value="">
                         <span class="help-block m-b-none">输入控制器/方法即可 例如:Rule/index</span>
                     </div>
@@ -111,9 +113,65 @@
 <script src="/WEB/Website/public/js/bootstrap.min.js?v=3.3.6"></script>
 <script src="/WEB/Website/public/js/content.min.js?v=1.0.0"></script>
 <script type="text/javascript">
-    function add(type){
+    function add(type,id,arr){
+        var str="";
         type=type+1;
+        if(Array.isArray(arr)) {
+        arr.map(function(i) {
+            str+=i+",";
+            });
+        console.log(str);
+        }else {
+            str=arr;
+        }
         $('input[name=type]').val(type);
+        $('input[name=fid]').val(id);
+        $('input[name=sort]').val(str);
+    }
+    function edit(id){
+        $('.modal-title').html("修改权限");  
+        $.ajax({
+            //几个参数需要注意一下
+            type: "POST",//方法类型
+            dataType: "json",//预期服务器返回的数据类型
+            url: "<?php echo U('Admin/updata');?>" ,//url
+            data: {id:id},
+            success: function (result) {
+                // console.log(result);//打印服务端返回的数据(调试用)
+                if (result.id == id) {
+                    $('input[name=title]').val(result.title);
+                    $('input[name=name]').val(result.name);
+                    $('input[name=type]').val(result.type);
+                    $('input[name=sort]').val(result.sort);
+                    $('input[name=id]').val(result.id);
+                }else{
+                    alert("数据错误");
+                }
+            },
+            error : function() {
+                alert("网络异常！");
+            }
+        }); 
+    }
+    function save (){
+        $.ajax({
+            //几个参数需要注意一下
+            type: "POST",//方法类型
+            dataType: "json",//预期服务器返回的数据类型
+            url: "<?php echo U('Admin/runadd');?>" ,//url
+            data: $('#qx').serialize(),
+            success: function (result) {
+                // console.log(result);//打印服务端返回的数据(调试用)
+                if (result.cod == 200) {
+                    window.location.reload();
+                }else{
+                    alert(result.msg);
+                }
+            },
+            error : function() {
+                alert("异常！");
+            }
+        });
     }
 </script>
 </body>
--- WEB/Website/Controller/AdminController.class.php
+++ WEB/Website/Controller/AdminController.class.php
@@ -10,8 +10,58 @@ Class AdminController extends CommonController{
 	}
 
 	public function rule (){
-		$this->rule=M('auth_rule')->order('sort asc,id asc')->select();
+		$rule=M('auth_rule')->field("*,concat(sort,',',id) as paths")->where(array('status'=>1))->order('paths')->select();
+		foreach($rule as $k=>$v){
+			$rule[$k]['pix']=str_repeat("|--",$v['type']-1);
+		}
+		$this->rule=$rule;
 		$this->display();
+	}
+
+	public function runadd (){
+		$db = M("auth_rule"); 
+		$lei = array(//自动验证
+	     array('name','require','规则必填'), 
+	     array('title','require','名称必填'),
+	     array('type','require','level级获取失败'),
+	     array('sort','require','排序获取失败')
+		);
+
+		$data['id'] = $_POST['id'];
+		$data['name'] = $_POST['name'];
+		$data['title'] = $_POST['title'];
+		$data['type'] = I('type','',intval);
+		$data['sort'] = $_POST['id'] ? $_POST['sort'] : $_POST['sort'].$_POST['fid'];
+
+		if (!$db->validate($lei)->create($data)){
+		     // 如果创建失败 表示验证没有通过
+		     $msg=$db->getError();
+		     $cod=202;
+		}else{
+			if($_POST['id']){
+				$rs=$db->save($data);
+			}else{
+				$rs=$db->add($data);
+			}
+			if($rs){$cod=200;}else{$msg="数据更新失败";$cod=201;}
+		}
+
+		$this->ajaxReturn(array('cod'=>$cod,'msg'=>$msg));
+	}
+
+	public function updata(){
+		$id=I('id');
+		$data=M('auth_rule')->where('id='.$id)->find();
+		$this->ajaxReturn($data);
+	}
+
+	public function delete (){
+		$id=I('id');
+		if(M('auth_rule')->where(array('id'=>$id))->delete()){
+			$this->success('删除成功！',U('Admin/rule'));
+		}else{
+			$this->error('删除失败！');
+		}
 	}
 }
 ?>
